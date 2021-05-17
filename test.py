af2f3d78451d4c5187551cef350c6ac8609b2a66
jay <jaylong255@gmail.com>
2021-05-17 09:12:15 -0500

    test

--- test.py
+++ test.py
@@ -12,73 +12,75 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-# GPIO.output(17, GPIO.LOW)
-#
+GPIO.output(17, GPIO.LOW)
+
 # time.sleep(0.25)
-#
-# GPIO.output(17, GPIO.HIGH)
+time.sleep(1)
+
+
+GPIO.output(17, GPIO.HIGH)
 
 # GPIO.output(17, GPIO.LOW)
-GPIO.output(17, GPIO.HIGH)
+# GPIO.output(17, GPIO.HIGH)
 
 
 GPIO.cleanup()
 
 # define the strike objects that specify the sound file and light pattern
 
-pygame.init()
-
-strikes = [
-    {
-        'thunder' : "thunder-01.mp3",
-        'lightning' :   "pattern 1"
-    },
-    {
-        'thunder' : "thunder-02.mp3",
-        'lightning' :   "pattern 2"
-    },
-    {
-        'thunder' : "thunder-03.mp3",
-        'lightning' :   "pattern 3"
-    },
-    {
-        'thunder' : "thunder-04.mp3",
-        'lightning' :   "pattern 4"
-    },
-    {
-        'thunder' : "thunder-05.mp3",
-        'lightning' :   "pattern 5"
-    },
-    {
-        'thunder' : "thunder-06.mp3",
-        'lightning' :   "pattern 6"
-    },
-    {
-        'thunder' : "thunder-07.mp3",
-        'lightning' :   "pattern 7"
-    },
-    {
-        'thunder' : "thunder-08.mp3",
-        'lightning' :   "pattern 8"
-    },
-    {
-        'thunder' : "thunder-09.mp3",
-        'lightning' :   "pattern 9"
-    },
-
-]
-
-# loop through each strike object
-
-while True:
-    for strike in strikes:
-
-        # play the sound file and trigger the relay according to the strike object's light pattern
-
-        print("playing thunder track: " + strike['thunder'])
-        print("flashing light pattern: " + strike['lightning'])
-        #playsound("Sounds/" + strike['thunder'])
-        pygame.mixer.music.load("Sounds/" + strike['thunder'])
-        pygame.mixer.music.play()
-        time.sleep(10)
-        pygame.mixer.music.stop()
+# pygame.init()
+#
+# strikes = [
+#     {
+#         'thunder' : "thunder-01.mp3",
+#         'lightning' :   "pattern 1"
+#     },
+#     {
+#         'thunder' : "thunder-02.mp3",
+#         'lightning' :   "pattern 2"
+#     },
+#     {
+#         'thunder' : "thunder-03.mp3",
+#         'lightning' :   "pattern 3"
+#     },
+#     {
+#         'thunder' : "thunder-04.mp3",
+#         'lightning' :   "pattern 4"
+#     },
+#     {
+#         'thunder' : "thunder-05.mp3",
+#         'lightning' :   "pattern 5"
+#     },
+#     {
+#         'thunder' : "thunder-06.mp3",
+#         'lightning' :   "pattern 6"
+#     },
+#     {
+#         'thunder' : "thunder-07.mp3",
+#         'lightning' :   "pattern 7"
+#     },
+#     {
+#         'thunder' : "thunder-08.mp3",
+#         'lightning' :   "pattern 8"
+#     },
+#     {
+#         'thunder' : "thunder-09.mp3",
+#         'lightning' :   "pattern 9"
+#     },
+#
+# ]
+#
+# # loop through each strike object
+#
+# while True:
+#     for strike in strikes:
+#
+#         # play the sound file and trigger the relay according to the strike object's light pattern
+#
+#         print("playing thunder track: " + strike['thunder'])
+#         print("flashing light pattern: " + strike['lightning'])
+#         #playsound("Sounds/" + strike['thunder'])
+#         pygame.mixer.music.load("Sounds/" + strike['thunder'])
+#         pygame.mixer.music.play()
+#         time.sleep(10)
+#         pygame.mixer.music.stop()
