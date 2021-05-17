0cfb99473502b553f788b3b2c4815c31dbe81b56
jay <jaylong255@gmail.com>
2021-05-16 21:16:18 -0500

    testing relay control

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
 import pygame, time
+import RPi.GPIO as GPIO
+
+# set up the relay pins
+
+# GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+#
+# RELAIS_1_GPIO = 17
+# GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
+# GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # out
+# GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
+time.sleep(0.25)
+
+GPIO.output(17, GPIO.HIGH)
+GPIO.cleanup()
+
+# define the strike objects that specify the sound file and light pattern
 
 pygame.init()
 
@@ -42,12 +63,17 @@ strikes = [
 
 ]
 
-while True:
-    for strike in strikes:
-        print("playing thunder track: " + strike['thunder'])
-        print("flashing light pattern: " + strike['lightning'])
-        #playsound("Sounds/" + strike['thunder'])
-        pygame.mixer.music.load("Sounds/" + strike['thunder'])
-        pygame.mixer.music.play()
-        time.sleep(10)
-        pygame.mixer.music.stop()
+# loop through each strike object
+
+# while True:
+#     for strike in strikes:
+#
+#         # play the sound file and trigger the relay according to the strike object's light pattern
+#
+#         print("playing thunder track: " + strike['thunder'])
+#         print("flashing light pattern: " + strike['lightning'])
+#         #playsound("Sounds/" + strike['thunder'])
+#         pygame.mixer.music.load("Sounds/" + strike['thunder'])
+#         pygame.mixer.music.play()
+#         time.sleep(10)
+#         pygame.mixer.music.stop()
