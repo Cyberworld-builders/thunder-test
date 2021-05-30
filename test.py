0297933fbbc4a94e4832ad4b691dbc83324a9046
jay <jaylong255@gmail.com>
2021-05-30 14:30:52 -0500

    forgot to save

--- test.py
+++ test.py
@@ -36,8 +36,6 @@ def pulse(duration, length):
         time.sleep(length)
         i += 1
 
-def flicker(duration)
-
 lightning = {
     'trackone' :   [
         # 1
@@ -206,13 +204,24 @@ while True:
         pygame.mixer.music.load(file)
         pygame.mixer.music.play()
 
-        for pattern in strike['patterns']:
-            print(patterns[pattern]['name'])
-            flash(patterns[pattern]['flash'])
-            # time.sleep(patterns[pattern]['flash'])
+        # for pattern in strike['patterns']:
+        #     print(patterns[pattern]['name'])
+        #     flash(patterns[pattern]['flash'])
+        #     # time.sleep(patterns[pattern]['flash'])
+        #
+        #     pause(patterns[pattern]['pause'])
+        #     # time.sleep(patterns[pattern]['pause'])
 
-            pause(patterns[pattern]['pause'])
-            # time.sleep(patterns[pattern]['pause'])
+
+        for strike in lightning.trackone:
+            if (strike.action == "flash"):
+                flash(strike.duration)
+            elif (strike.action == "crackle"):
+                pulse(strike.duration, .2)
+            elif (strike.action == "flicker"):
+                pulse(strike.duration, .1)
+            else:
+                pause(strike.duration)
 
         secondsPassed = time.time() - startTime
         print("seconds passed: " + str(round(secondsPassed,2)))
